86216b28900d7975e1c6c7c49bfdb3fd9c5a7cf4
Tiago Novaes <tiagonsousadev@gmail.com>
2025-04-21 11:47:27 -0300

    First Commit

---
+++ src/main/java/com/t_evolution_Systems/todo_list/TodoListApplication.java
@@ -0,0 +1,13 @@
+package com.t_evolution_Systems.todo_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/t_evolution_Systems/todo_list/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t_evolution_Systems.todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
